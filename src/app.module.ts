129d4e4424b6d48cbae3a21ece3f3ef38dfc4897
itachi <enchantinggg4@gmail.com>
2020-10-17 15:03:18 +0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CqrsModule } from '@nestjs/cqrs';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { devDbConfig, Entities, prodDbConfig } from './config/typeorm.config';
+import { isDev, REDIS_URL } from './config/env';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    CqrsModule,
+    TypeOrmModule.forRoot(
+      (isDev ? devDbConfig : prodDbConfig) as TypeOrmModuleOptions,
+    ),
+    TypeOrmModule.forFeature(Entities),
+    ClientsModule.register([
+      {
+        name: 'QueryCore',
+        transport: Transport.REDIS,
+        options: {
+          url: REDIS_URL(),
+          retryAttempts: 10,
+          retryDelay: 5000,
+        },
+      },
+    ] as any),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,29 @@
+import {DB_HOST, DB_PASSWORD, DB_USERNAME} from "./env";
+
+export const Entities = [
+
+]
+export const devDbConfig: any = {
+  type: 'postgres',
+  database: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  entities: Entities,
+  synchronize: true,
+
+  keepConnectionAlive: true,
+};
+
+export const prodDbConfig: any = {
+  type: 'postgres',
+  database: 'postgres',
+  host: DB_HOST(),
+  port: 5432,
+  username: DB_USERNAME(),
+  password: DB_PASSWORD(),
+  entities: Entities,
+  synchronize: true,
+  ssl: false,
+};
