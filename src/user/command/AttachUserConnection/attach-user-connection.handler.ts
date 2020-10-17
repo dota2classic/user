693ffc821dee0a5be6f74b44bce7d054dcefadb2
itachi <enchantinggg4@gmail.com>
2020-10-17 15:57:50 +0300

    Some events and attach connection command handler

---
+++ src/user/command/AttachUserConnection/attach-user-connection.handler.ts
@@ -0,0 +1,60 @@
+import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
+import { Logger } from '@nestjs/common';
+import { AttachUserConnectionCommand } from '../../../gateway/commands/attach-user-connection.command';
+import { UserConnectionEntity } from '../../model/user-connection.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../../model/user.entity';
+import { UserConnectionCreatedEvent } from '../../../gateway/events/user/user-connection-created.event';
+import { PlayerId } from '../../../gateway/shared-types/player-id';
+import { UserCreatedEvent } from '../../../gateway/events/user/user-created.event';
+
+@CommandHandler(AttachUserConnectionCommand)
+export class AttachUserConnectionHandler
+  implements ICommandHandler<AttachUserConnectionCommand> {
+  private readonly logger = new Logger(AttachUserConnectionHandler.name);
+
+  constructor(
+    @InjectRepository(UserConnectionEntity)
+    private readonly userConnectionEntityRepository: Repository<
+      UserConnectionEntity
+    >,
+    @InjectRepository(UserEntity)
+    private readonly userEntityRepository: Repository<UserEntity>,
+    private readonly ebus: EventBus,
+  ) {}
+
+  async execute(command: AttachUserConnectionCommand) {
+    let u = await this.userEntityRepository.findOne({
+      steam_id: command.playerId.value,
+    });
+    if (!u) {
+      u = new UserEntity();
+      u.steam_id = command.playerId.value;
+      await this.userEntityRepository.save(u);
+      this.ebus.publish(new UserCreatedEvent(command.playerId));
+    }
+
+    const existingConnection = await this.userConnectionEntityRepository.findOne(
+      {
+        steam_id: u.steam_id,
+        connection: command.connection,
+        external_id: command.externalId,
+      },
+    );
+    if (!existingConnection) {
+      const con = new UserConnectionEntity();
+      con.steam_id = u.steam_id;
+      con.connection = command.connection;
+      con.external_id = command.externalId;
+      await this.userConnectionEntityRepository.save(con);
+      this.ebus.publish(
+        new UserConnectionCreatedEvent(
+          new PlayerId(con.steam_id),
+          con.connection,
+          con.external_id,
+        ),
+      );
+    }
+  }
+}
--- src/user/index.ts
+++ src/user/index.ts
@@ -1,10 +1,11 @@
 import { GetByConnectionHandler } from './query/GetByConnection/get-by-connection.handler';
+import { AttachUserConnectionHandler } from './command/AttachUserConnection/attach-user-connection.handler';
 
 const Sagas = [];
 const EventHandlers = [];
 const Repositories = [];
 const QueryHandlers = [GetByConnectionHandler];
-const CommandHandlers = [];
+const CommandHandlers = [AttachUserConnectionHandler];
 export const UserProviders = [
   ...CommandHandlers,
   ...Sagas,
