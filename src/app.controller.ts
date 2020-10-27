a645846f82593c12caca5f452da9a705da3a23ee
itachi <enchantinggg4@gmail.com>
2020-10-27 23:00:07 +0300

    handlers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,10 @@ import { construct } from './gateway/util/construct';
 import { QueryBus } from '@nestjs/cqrs';
 import { GetAllConnectionsQuery } from 'src/gateway/queries/GetAllConnections/get-all-connections.query';
 import { GetAllConnectionsQueryResult } from 'src/gateway/queries/GetAllConnections/get-all-connections-query.result';
+import { GetAllQuery } from './gateway/queries/GetAll/get-all.query';
+import { GetAllQueryResult } from 'src/gateway/queries/GetAll/get-all-query.result';
+import { GetUserInfoQueryResult } from 'src/gateway/queries/GetUserInfo/get-user-info-query.result';
+import { GetUserInfoQuery } from './gateway/queries/GetUserInfo/get-user-info.query';
 
 @Controller()
 export class AppController {
@@ -23,5 +27,20 @@ export class AppController {
     query: GetAllConnectionsQuery,
   ): Promise<GetAllConnectionsQueryResult> {
     return this.qbus.execute(construct(GetAllConnectionsQuery, query));
+  }
+
+  @MessagePattern(GetAllQuery.name)
+  async GetAllQuery(
+    query: GetAllQuery,
+  ): Promise<GetAllQueryResult> {
+    return this.qbus.execute(construct(GetAllQuery, query));
+  }
+
+
+  @MessagePattern(GetUserInfoQuery.name)
+  async GetUserInfoQuery(
+    query: GetUserInfoQuery,
+  ): Promise<GetUserInfoQueryResult> {
+    return this.qbus.execute(construct(GetUserInfoQuery, query));
   }
 }
--- src/user/index.ts
+++ src/user/index.ts
@@ -2,6 +2,7 @@ import { GetByConnectionHandler } from './query/GetByConnection/get-by-connectio
 import { AttachUserConnectionHandler } from './command/AttachUserConnection/attach-user-connection.handler';
 import { GetAllConnectionsHandler } from 'src/user/query/GetAllConnections/get-all-connections.handler';
 import { GetAllHandler } from 'src/user/query/GetAll/get-all.handler';
+import { GetUserInfoHandler } from 'src/user/query/GetUserInfo/get-user-info.handler';
 
 const Sagas = [];
 const EventHandlers = [];
@@ -10,6 +11,7 @@ const QueryHandlers = [
   GetByConnectionHandler,
   GetAllConnectionsHandler,
   GetAllHandler,
+  GetUserInfoHandler
 ];
 const CommandHandlers = [AttachUserConnectionHandler];
 export const UserProviders = [
