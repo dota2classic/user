ad167c995c31881e60c7b205fc89a9fdf0c02cf4
Yuri Stepin <enchantinggg4@gmail.com>
2025-04-22 01:25:12 +0300

    Fix role update not updating cache

--- src/user/event-handler/role-lifetime-updated.handler.ts
+++ src/user/event-handler/role-lifetime-updated.handler.ts
@@ -1,26 +1,21 @@
-import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
+import { EventBus, EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { RoleLifetimeUpdatedEvent } from 'src/user/event/role-lifetime-updated.event';
-import { UserRoleLifetimeEntity } from 'src/user/model/user-role-lifetime.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/model/user.entity';
+import { UserUpdatedInnerEvent } from 'src/user/event/user-updated-inner.event';
 
 @EventsHandler(RoleLifetimeUpdatedEvent)
 export class RoleLifetimeUpdatedHandler
-  implements IEventHandler<RoleLifetimeUpdatedEvent> {
+  implements IEventHandler<RoleLifetimeUpdatedEvent>
+{
   constructor(
-    @InjectRepository(UserRoleLifetimeEntity)
-    private readonly userRoleLifetimeEntityRepository: Repository<
-      UserRoleLifetimeEntity
-    >,
+    @InjectRepository(UserEntity)
+    private readonly userEntityRepository: Repository<UserEntity>,
+    private readonly ebus: EventBus,
   ) {}
 
   async handle(event: RoleLifetimeUpdatedEvent) {
-    const lft = await this.userRoleLifetimeEntityRepository.findOne({
-      where: {
-        steam_id: event.steam_id,
-        role: event.role,
-      }
-    });
-    if (!lft) return;
+    this.ebus.publish(new UserUpdatedInnerEvent(event.steam_id));
   }
 }
